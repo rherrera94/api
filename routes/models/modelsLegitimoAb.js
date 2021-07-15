32a421215ceff276b8a6beae3ed1aee7e087cc0d
rherrera94 <rafael.herrera@outlook.com.ar>
2021-07-15 15:40:53 -0300

    centralizo la conexion a bbdd

--- routes/models/modelsLegitimoAb.js
+++ routes/models/modelsLegitimoAb.js
@@ -1,27 +1,4 @@
-require('dotenv').config({ path:'../../.env'});
-/********************CONEXIÓN A BASE DE DATOS*************************/
-/*   Se utilizan variables de entorno para ocultar datos sensibles.  */
-/*********************************************************************/
-var mysql=require('mysql');
-const { REPL_MODE_SLOPPY } = require('repl');
-const util=require ('util'); // no se necesita instalarla
-
-var conexion= mysql.createConnection({
-    host: process.env.APP_HOST,
-    user: process.env.APP_USER,
-    password:process.env.APP_KEY,
-    database:process.env.APP_DB
-});
-
-conexion.connect((error)=>{
-    if(error){
-        console.log("Error en la conexion a la Base de datos");
-        return;
-    }
-});
-const qy= util.promisify(conexion.query).bind(conexion); //permitira el uso de async-await en la conexion mysql
-/*********************************************************************/
-
+const qy=require('./conexion');
 /**
  * @param {object} legitimoAbono objeto con la informacion del legitimo abono a agregar
  * @returns {Array} Devuelve la información que fue cargada en la base de datos o de haber algun
