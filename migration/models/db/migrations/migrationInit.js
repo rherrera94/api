55f1ea1f748fa0b1f0bd1d4d673f4672ac795210
rherrera94 <rafael.herrera@outlook.com.ar>
2021-10-31 16:48:37 -0300

    .

--- migration/models/db/migrations/migrationInit.js
+++ migration/models/db/migrations/migrationInit.js
@@ -219,19 +219,19 @@ const bcrypt= require('bcrypt');
                 id,
                 role_id
             })
-            let id="PERMIT_LOGIN";
+            id="PERMIT_LOGIN";
             const role_id=2;
             await db("permiso").insert({
                 id,
                 role_id
             })
-            let id="PERMIT_LOGIN";
+            id="PERMIT_LOGIN";
             const role_id=3;
             await db("permiso").insert({
                 id,
                 role_id
             })
-            let id="PERMIT_LOGIN";
+            id="PERMIT_LOGIN";
             const role_id=4;
             await db("permiso").insert({
                 id,
